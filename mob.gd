de7a561c58ad75425fc8156bf73bbb76b8b0f09a
Simon Olander Sahlén <simon.olander-sahlen@regionstockholm.se>
2023-10-17 11:10:10 +0200

    Mobs should not look up

--- mob.gd
+++ mob.gd
@@ -11,6 +11,7 @@ func _physics_process(_delta):
 func initialize(start_position, player_position):
 	var speed = randi_range(min_speed, max_speed)
 	look_at_from_position(start_position, player_position)
+	rotation.x = 0 # We don't want them to look up if the player's in the air
 	rotate_y(randf_range(-PI/4, PI/4))
 	velocity = Vector3.FORWARD * speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
